e4351b22b2b0d253e3cd5d9f4476d5e4f3852084
Cahllagerfeld <Cahllagerfeld>
2021-12-18 14:08:47 +0000

    feat: move logic from generate.js to api-endpoint

--- pages/api/profiles.js
+++ pages/api/profiles.js
@@ -1,5 +1,24 @@
-import data from "../../.cache/data.json";
+import fs from "fs";
+import path from "path";
 
-export default function handler(req, res) {
-  res.status(200).json(data)
+export default async function handler(req, res) {
+  const relativeDir = path.resolve("./public", "profiles");
+  const files = fs.readdirSync(relativeDir);
+
+  const profiles = await Promise.all(
+    files.map(async (file) => {
+      const response = await fetch(
+        `http://localhost:3000${path.join("/", "profiles", file)}`
+      );
+      if (response.ok) {
+        const data = await response.json();
+        return { username: file.split(".")[0], ...data };
+      }
+    })
+  );
+
+  const output = profiles.map((profile) => ({
+    ...profile,
+  }));
+  res.status(200).json(output);
 }
