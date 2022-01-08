b983cf0fbe511b3a1c024078294c952da5c0cba0
Cahllagerfeld <Cahllagerfeld>
2022-01-08 20:49:16 +0000

    feat: add colors to tailwind-config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        chili: "#C23100",
+        "international-orange": "#FF5A00",
+        koromiko: "#FFb95E",
+        "dark-metal": "#202328",
+        "dark-granite": "#65615E",
+        "light-granite": "#D4D1C5",
+        parsley: "#1D531D",
+        "apple-green": "#3DA639",
+      },
+    },
+  },
   plugins: [],
 };
